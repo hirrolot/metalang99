db239ac582c0ee289907e21feaf86e4364cecbe8
hirrolot <hirrolot@gmail.com>
2020-09-19 04:55:10 +0600

    Add include/koshmar.h

---
+++ include/koshmar.h
@@ -0,0 +1,12 @@
+#ifndef KOSHMAR_H
+#define KOSHMAR_H
+
+#include "koshmar/call_macro.h"
+#include "koshmar/cat.h"
+#include "koshmar/emptiness.h"
+#include "koshmar/overload.h"
+#include "koshmar/rec.h"
+#include "koshmar/stringify.h"
+#include "koshmar/variadics.h"
+
+#endif // KOSHMAR_H
