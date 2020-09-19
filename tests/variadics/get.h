bbea9f971f63871c366e43e76eac6f49b450a3ba
hirrolot <hirrolot@gmail.com>
2020-09-20 02:32:38 +0600

    Don't compare floating-point numbers

--- tests/variadics/get.h
+++ tests/variadics/get.h
@@ -4,9 +4,9 @@
 #include <string.h>
 
 {
-#define ARGS 87.2157, "Hello", 123, NULL, bla *bla - bla
+#define ARGS 872157, "Hello", 123, NULL, bla *bla - bla
 
-    assert(KOSHMAR_PP_CALL_MACRO(KOSHMAR_PP_VARIADICS_HEAD, ARGS) == 87.2157);
+    assert(KOSHMAR_PP_CALL_MACRO(KOSHMAR_PP_VARIADICS_HEAD, ARGS) == 872157);
     assert(KOSHMAR_PP_VARIADICS_HEAD(981, ) == 981);
 
 #define CHECK_TAIL(hello, _123, null, number)                                                      \
