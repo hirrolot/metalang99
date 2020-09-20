f978b19ca25c7b1924effc6d83e1cd68fd11eedb
hirrolot <hirrolot@gmail.com>
2020-09-20 20:47:06 +0600

    Add KOSHMAR_PP_NUMERICS_INC, KOSHMAR_PP_NUMERICS_DEC, KOSHMAR_PP_NUMERICS_EQ, KOSHMAR_PP_NUMERICS_MAX

--- include/koshmar_pp.h
+++ include/koshmar_pp.h
@@ -4,6 +4,7 @@
 #include "koshmar_pp/aux.h"
 #include "koshmar_pp/if_then_else.h"
 #include "koshmar_pp/logical.h"
+#include "koshmar_pp/numerics.h"
 #include "koshmar_pp/overload.h"
 #include "koshmar_pp/rec.h"
 #include "koshmar_pp/variadics.h"
