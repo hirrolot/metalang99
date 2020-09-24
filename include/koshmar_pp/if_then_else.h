cfbf43ccff6489de9a50ba507a5e8bac6d717e66
hirrolot <hirrolot@gmail.com>
2020-09-24 19:32:40 +0600

    Allow commas in KOSHMAR_PP_IF

--- include/koshmar_pp/if_then_else.h
+++ include/koshmar_pp/if_then_else.h
@@ -3,9 +3,12 @@
 
 #include "aux.h"
 
-#define KOSHMAR_PP_IF(cond, x, y) KOSHMAR_PP_CAT(KOSHMAR_PP_PRIVATE_IF_, cond)(x, y)
+#define KOSHMAR_PP_IF(cond, x, y)                                                                  \
+    KOSHMAR_PP_PRIVATE_IF_EXTRACT KOSHMAR_PP_CAT(KOSHMAR_PP_PRIVATE_IF_, cond)((x), (y))
 
 #define KOSHMAR_PP_PRIVATE_IF_0(_x, y) y
 #define KOSHMAR_PP_PRIVATE_IF_1(x, _y) x
+
+#define KOSHMAR_PP_PRIVATE_IF_EXTRACT(...) __VA_ARGS__
 
 #endif // KOSHMAR_PP_IF_THEN_ELSE_H
