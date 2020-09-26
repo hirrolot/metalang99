a22ecdf296bc1c4bdfcaf670fe72036fa94ff9de
hirrolot <hirrolot@gmail.com>
2020-09-26 16:31:13 +0600

    Test KOSHMAR_PP_WHILE for 0 iterations

--- tests/control/while.h
+++ tests/control/while.h
@@ -14,3 +14,13 @@ TEST(0 KOSHMAR_PP_WHILE(PREDICATE, OP, 1) == SUM);
 #undef OP
 #undef LIMIT
 #undef SUM
+
+#define PREDICATE(i) 0
+#define OP(i)        it will never be executed
+#define EMPTY_WHILE  KOSHMAR_PP_WHILE(PREDICATE, OP, blah)
+
+EMPTY_WHILE static EMPTY_WHILE int EMPTY_WHILE test_empty_while EMPTY_WHILE =
+    EMPTY_WHILE 1 EMPTY_WHILE;
+
+#undef PREDICATE
+#undef OP
