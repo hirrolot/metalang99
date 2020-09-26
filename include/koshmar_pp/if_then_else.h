928e5e89315642a8643c6a45daff56793629071d
hirrolot <hirrolot@gmail.com>
2020-09-26 10:42:14 +0600

    Use KOSHMAR_PP_UNPARENTHESISE in KOSHMAR_PP_IF

--- include/koshmar_pp/if_then_else.h
+++ include/koshmar_pp/if_then_else.h
@@ -20,13 +20,10 @@
 #ifndef DOXYGEN_SHOULD_SKIP_THIS
 
 #define KOSHMAR_PP_PRIVATE_IF(cond, x, y)                                                          \
-    KOSHMAR_PP_EXPAND(                                                                             \
-        KOSHMAR_PP_PRIVATE_IF_EXTRACT KOSHMAR_PP_CAT(KOSHMAR_PP_PRIVATE_IF_, cond)((x), (y)))
+    KOSHMAR_PP_UNPARENTHESISE(KOSHMAR_PP_CAT(KOSHMAR_PP_PRIVATE_IF_, cond)((x), (y)))
 
 #define KOSHMAR_PP_PRIVATE_IF_0(_x, y) y
 #define KOSHMAR_PP_PRIVATE_IF_1(x, _y) x
-
-#define KOSHMAR_PP_PRIVATE_IF_EXTRACT(...) __VA_ARGS__
 
 #endif // DOXYGEN_SHOULD_SKIP_THIS
 
