3a8c26b1192bcf2f586f1b77b299143556d0fb34
hirrolot <hirrolot@gmail.com>
2020-12-15 00:55:24 +0600

    Adjust the evaluator with the spec

--- include/epilepsy/eval.h
+++ include/epilepsy/eval.h
@@ -32,12 +32,9 @@
 #ifndef DOXYGEN_SHOULD_SKIP_THIS
 
 #define EPILEPSY_PRIV_EVAL(...)                                                                    \
-    EPILEPSY_PRIV_EVAL_REC_UNROLL(                                                                 \
-        EPILEPSY_PRIV_EVAL_AUX(EPILEPSY_PRIV_EVAL_REC_STOP, (~), __VA_ARGS__))
-
-#define EPILEPSY_PRIV_EVAL_AUX(k, k_cx, ...)                                                       \
-    EPILEPSY_PRIV_EVAL_MATCH(                                                                      \
-        k, k_cx, EPILEPSY_PRIV_EVAL_ACC_EMPTY(), __VA_ARGS__ EPILEPSY_PRIV_EVAL_TERM_END(), ~)
+    EPILEPSY_PRIV_EVAL_REC_UNROLL(EPILEPSY_PRIV_EVAL_MATCH(                                        \
+        EPILEPSY_PRIV_EVAL_REC_STOP, (~), EPILEPSY_PRIV_EVAL_ACC_EMPTY(),                          \
+        __VA_ARGS__ EPILEPSY_PRIV_EVAL_TERM_END(), ~))
 
 #define EPILEPSY_PRIV_EVAL_MATCH(...) EPILEPSY_PRIV_EVAL_MATCH_AUX(__VA_ARGS__)
 #define EPILEPSY_PRIV_EVAL_MATCH_AUX(k, k_cx, acc, head, ...)                                      \
@@ -45,26 +42,26 @@
 
 #define EPILEPSY_PRIV_EVAL_MATCH_v(k, k_cx, acc, tail, ...)                                        \
     EPILEPSY_PRIV_EVAL_MACHINE_OP_CONTINUE(                                                        \
-        k, k_cx, EPILEPSY_PRIV_EVAL_ACC_EXTEND(acc, __VA_ARGS__), tail)
+        k, k_cx, EPILEPSY_PRIV_EVAL_ACC_EXTEND(acc, __VA_ARGS__),                                  \
+        EPILEPSY_PRIV_EVAL_ACC_UNWRAP(tail))
 
 #define EPILEPSY_PRIV_EVAL_MATCH_op(k, k_cx, acc, tail, op, ...)                                   \
-    EPILEPSY_PRIV_EVAL_REC_CONTINUE(                                                               \
-        EPILEPSY_PRIV_EVAL_AUX_HOOK,                                                               \
-        (EPILEPSY_PRIV_EVAL_MATCH_op_TRANSFORM_HOOK, (k, k_cx, acc, tail, op)),                    \
-        EPILEPSY_PRIV_EVAL_PATCH_TERMS_WITH_COMMAS(__VA_ARGS__))
+    EPILEPSY_PRIV_EVAL_MACHINE_OP_CONTINUE(                                                        \
+        EPILEPSY_PRIV_EVAL_MATCH_op_TRANSFORM_HOOK, (k, k_cx, acc, tail, op),                      \
+        EPILEPSY_PRIV_EVAL_ACC_EMPTY(),                                                            \
+        EPILEPSY_PRIV_EVAL_PATCH_TERMS_WITH_COMMAS(__VA_ARGS__) EPILEPSY_PRIV_EVAL_TERM_END(), ~)
 
 #define EPILEPSY_PRIV_EVAL_MATCH_args(k, k_cx, acc, tail, op, ...)                                 \
-    EPILEPSY_PRIV_EVAL_REC_CONTINUE(                                                               \
-        EPILEPSY_PRIV_EVAL_AUX_HOOK,                                                               \
-        (EPILEPSY_PRIV_EVAL_MATCH_args_TRANSFORM_HOOK, (k, k_cx, acc, tail, (__VA_ARGS__))), op,   \
-        EPILEPSY_PRIV_AUX_EMPTY())
+    EPILEPSY_PRIV_EVAL_MACHINE_OP_CONTINUE(                                                        \
+        EPILEPSY_PRIV_EVAL_MATCH_args_TRANSFORM_HOOK, (k, k_cx, acc, tail, (__VA_ARGS__)),         \
+        EPILEPSY_PRIV_EVAL_ACC_EMPTY(), op, EPILEPSY_PRIV_EVAL_TERM_END(), ~)
 
 #define EPILEPSY_PRIV_EVAL_MATCH_end(k, k_cx, acc, _tail, _)                                       \
     EPILEPSY_PRIV_EVAL_MACHINE_OP_STOP(k, k_cx, acc)
 
 #define EPILEPSY_PRIV_EVAL_MATCH_op_TRANSFORM(k, k_cx, acc, tail, evaluated_op, ...)               \
     EPILEPSY_PRIV_EVAL_MACHINE_OP_CONTINUE(                                                        \
-        k, k_cx, acc, (evaluated_op(__VA_ARGS__) EPILEPSY_PRIV_EVAL_CONTROL_UNWRAP(tail)))
+        k, k_cx, acc, evaluated_op(__VA_ARGS__) EPILEPSY_PRIV_EVAL_CONTROL_UNWRAP(tail))
 
 #define EPILEPSY_PRIV_EVAL_MATCH_args_TRANSFORM(k, k_cx, acc, tail, args, evaluated_op)            \
     EPILEPSY_PRIV_EVAL_MATCH(                                                                      \
