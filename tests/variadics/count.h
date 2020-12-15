ba93d30a32c99881c75f814385aba44d94073ed8
hirrolot <hirrolot@gmail.com>
2020-12-15 12:05:49 +0600

    Allow at most 256 arguments per macro

--- tests/variadics/count.h
+++ tests/variadics/count.h
@@ -3,41 +3,28 @@
 #include <epilepsy/lang.h>
 #include <epilepsy/variadics.h>
 
+#define _5_ARGS   v(~) v(~) v(~) v(~) v(~)
+#define _10_ARGS  _5_ARGS _5_ARGS
+#define _50_ARGS  _10_ARGS _10_ARGS _10_ARGS _10_ARGS _10_ARGS
+#define _100_ARGS _50_ARGS _50_ARGS
+
 EPILEPSY_ASSERT_EQ(EPILEPSY_VARIADICS_COUNT(), v(1));
 EPILEPSY_ASSERT_EQ(EPILEPSY_VARIADICS_COUNT(v(~)), v(1));
 EPILEPSY_ASSERT_EQ(EPILEPSY_VARIADICS_COUNT(v(~) v(~)), v(2));
 EPILEPSY_ASSERT_EQ(EPILEPSY_VARIADICS_COUNT(v(~) v(~) v(~)), v(3));
 EPILEPSY_ASSERT_EQ(EPILEPSY_VARIADICS_COUNT(v(~) v(~) v(~) v(~)), v(4));
-EPILEPSY_ASSERT_EQ(EPILEPSY_VARIADICS_COUNT(v(~) v(~) v(~) v(~) v(~)), v(5));
-EPILEPSY_ASSERT_EQ(EPILEPSY_VARIADICS_COUNT(v(~) v(~) v(~) v(~) v(~) v(~)), v(6));
-EPILEPSY_ASSERT_EQ(EPILEPSY_VARIADICS_COUNT(v(~) v(~) v(~) v(~) v(~) v(~) v(~)), v(7));
-EPILEPSY_ASSERT_EQ(EPILEPSY_VARIADICS_COUNT(v(~) v(~) v(~) v(~) v(~) v(~) v(~) v(~)), v(8));
-EPILEPSY_ASSERT_EQ(EPILEPSY_VARIADICS_COUNT(v(~) v(~) v(~) v(~) v(~) v(~) v(~) v(~) v(~)), v(9));
-EPILEPSY_ASSERT_EQ(
-    EPILEPSY_VARIADICS_COUNT(v(~) v(~) v(~) v(~) v(~) v(~) v(~) v(~) v(~) v(~)), v(10));
-EPILEPSY_ASSERT_EQ(
-    EPILEPSY_VARIADICS_COUNT(v(~) v(~) v(~) v(~) v(~) v(~) v(~) v(~) v(~) v(~) v(~)), v(11));
-EPILEPSY_ASSERT_EQ(
-    EPILEPSY_VARIADICS_COUNT(v(~) v(~) v(~) v(~) v(~) v(~) v(~) v(~) v(~) v(~) v(~) v(~) v(~) v(~)
-                                 v(~) v(~) v(~) v(~) v(~) v(~) v(~) v(~) v(~) v(~) v(~) v(~) v(~)
-                                     v(~) v(~) v(~) v(~) v(~) v(~) v(~) v(~) v(~) v(~) v(~) v(~)
-                                         v(~) v(~) v(~) v(~) v(~) v(~) v(~) v(~) v(~) v(~) v(~) v(~)
-                                             v(~) v(~) v(~) v(~) v(~) v(~) v(~) v(~) v(~) v(~) v(~)
-                                                 v(~) v(~) v(~) v(~) v(~) v(~) v(~) v(~) v(~) v(~)
-                                                     v(~) v(~) v(~) v(~) v(~) v(~) v(~) v(~) v(~)
-                                                         v(~) v(~) v(~) v(~)),
-    v(85));
+EPILEPSY_ASSERT_EQ(EPILEPSY_VARIADICS_COUNT(_5_ARGS), v(5));
+EPILEPSY_ASSERT_EQ(EPILEPSY_VARIADICS_COUNT(_5_ARGS v(~)), v(6));
+EPILEPSY_ASSERT_EQ(EPILEPSY_VARIADICS_COUNT(_5_ARGS v(~) v(~)), v(7));
+EPILEPSY_ASSERT_EQ(EPILEPSY_VARIADICS_COUNT(_5_ARGS v(~) v(~) v(~)), v(8));
+EPILEPSY_ASSERT_EQ(EPILEPSY_VARIADICS_COUNT(_5_ARGS v(~) v(~) v(~) v(~)), v(9));
+EPILEPSY_ASSERT_EQ(EPILEPSY_VARIADICS_COUNT(_10_ARGS), v(10));
+EPILEPSY_ASSERT_EQ(EPILEPSY_VARIADICS_COUNT(_10_ARGS v(~)), v(11));
+EPILEPSY_ASSERT_EQ(EPILEPSY_VARIADICS_COUNT(_50_ARGS _10_ARGS _5_ARGS v(~)), v(66));
+EPILEPSY_ASSERT_EQ(EPILEPSY_VARIADICS_COUNT(_100_ARGS _50_ARGS v(~) v(~)), v(152));
+EPILEPSY_ASSERT_EQ(EPILEPSY_VARIADICS_COUNT(_100_ARGS _100_ARGS _50_ARGS _5_ARGS v(~)), v(256));
 
-EPILEPSY_ASSERT_EQ(
-    EPILEPSY_VARIADICS_COUNT(
-        v(~) v(~) v(~) v(~) v(~) v(~) v(~) v(~) v(~) v(~) v(~) v(~) v(~) v(~) v(~) v(~) v(~) v(~)
-            v(~) v(~) v(~) v(~) v(~) v(~) v(~) v(~) v(~) v(~) v(~) v(~) v(~) v(~) v(~) v(~) v(~)
-                v(~) v(~) v(~) v(~) v(~) v(~) v(~) v(~) v(~) v(~) v(~) v(~) v(~) v(~) v(~) v(~) v(~)
-                    v(~) v(~) v(~) v(~) v(~) v(~) v(~) v(~) v(~) v(~) v(~) v(~) v(~) v(~) v(~) v(~)
-                        v(~) v(~) v(~) v(~) v(~) v(~) v(~) v(~) v(~) v(~) v(~) v(~) v(~) v(~) v(~)
-                            v(~) v(~) v(~) v(~) v(~) v(~) v(~) v(~) v(~) v(~) v(~) v(~) v(~) v(~)
-                                v(~) v(~) v(~) v(~) v(~) v(~) v(~) v(~) v(~) v(~) v(~) v(~) v(~)
-                                    v(~) v(~) v(~) v(~) v(~) v(~) v(~) v(~) v(~) v(~) v(~) v(~) v(~)
-                                        v(~) v(~) v(~) v(~) v(~) v(~) v(~) v(~) v(~) v(~) v(~) v(~)
-                                            v(~) v(~)),
-    v(137));
+#undef _5_ARGS
+#undef _10_ARGS
+#undef _50_ARGS
+#undef _100_ARGS
