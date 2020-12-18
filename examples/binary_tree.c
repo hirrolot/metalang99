f073ec5cf33c7c13e76d809f4dfb835afa089abc
hirrolot <hirrolot@gmail.com>
2020-12-19 02:02:00 +0600

    Reorder the definitions in examples/binary_tree.c

--- examples/binary_tree.c
+++ examples/binary_tree.c
@@ -2,14 +2,6 @@
 
 #include <stdio.h>
 
-#define TreeNode_REAL(lhs, data, rhs) EPILEPSY_CHOICE(v(TreeNode) Node(v(lhs), v(data), v(rhs)))
-#define TreeLeaf_REAL(x)              EPILEPSY_CHOICE(v(TreeLeaf) v(x))
-
-#define Node_REAL(lhs, data, rhs) EPILEPSY_RECORD(v(lhs) v(data) v(rhs))
-#define NodeLhs_REAL(node)        EPILEPSY_RECORD_GET(v(node), v(0))
-#define NodeData_REAL(node)       EPILEPSY_RECORD_GET(v(node), v(1))
-#define NodeRhs_REAL(node)        EPILEPSY_RECORD_GET(v(node), v(2))
-
 #define TreeNode(lhs, data, rhs) call(TreeNode_REAL, lhs data rhs)
 #define TreeLeaf(x)              call(TreeLeaf_REAL, x)
 
@@ -18,7 +10,16 @@
 #define NodeData(node)       call(NodeData_REAL, node)
 #define NodeRhs(node)        call(NodeRhs_REAL, node)
 
-#define SUM(tree)          call(SUM_REAL, tree)
+#define SUM(tree) call(SUM_REAL, tree)
+
+#define TreeNode_REAL(lhs, data, rhs) EPILEPSY_CHOICE(v(TreeNode) Node(v(lhs), v(data), v(rhs)))
+#define TreeLeaf_REAL(x)              EPILEPSY_CHOICE(v(TreeLeaf) v(x))
+
+#define Node_REAL(lhs, data, rhs) EPILEPSY_RECORD(v(lhs) v(data) v(rhs))
+#define NodeLhs_REAL(node)        EPILEPSY_RECORD_GET(v(node), v(0))
+#define NodeData_REAL(node)       EPILEPSY_RECORD_GET(v(node), v(1))
+#define NodeRhs_REAL(node)        EPILEPSY_RECORD_GET(v(node), v(2))
+
 #define SUM_REAL(tree)     EPILEPSY_MATCH(v(tree), v(SUM_))
 #define SUM_TreeNode(node) SUM(NodeLhs(v(node))) v(+) NodeData(v(node)) v(+) SUM(NodeRhs(v(node)))
 #define SUM_TreeLeaf(x)    v(x)
