24571fe2d54eb95c96173f643bea41d1b0ab8c9a
hirrolot <hirrolot@gmail.com>
2020-12-19 12:59:57 +0600

    Format examples/ too

--- check-fmt.sh
+++ check-fmt.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-./run-clang-format/run-clang-format.py -r include tests
+./run-clang-format/run-clang-format.py -r include tests examples
--- fmt.sh
+++ fmt.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-find include tests -iname *.h -o -iname *.c | xargs clang-format -i
+find include tests examples -iname *.h -o -iname *.c | xargs clang-format -i
