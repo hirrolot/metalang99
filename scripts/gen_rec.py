69aaf01225aeb84a04c1f9b59ea8ed3b2b35e29f
hirrolot <hirrolot@gmail.com>
2020-12-20 16:53:30 +0600

    Re-write scripts/gen_rec.c to Python3

---
+++ scripts/gen_rec.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python3
+
+# This script generates recursion unrollers.
+# Usage: ./scripts/gen_rec.py <start-lvl> <count> <output-file-name> <header-guard>
+
+import sys
+
+start_lvl = int(sys.argv[1])
+count = int(sys.argv[2])
+output_file_name = sys.argv[3]
+header_guard = sys.argv[4]
+
+f = open(output_file_name, "w")
+
+f.write(
+    f"""
+#ifndef {header_guard}
+#define {header_guard}
+
+#include <epilepsy/eval/rec/progress.h>
+
+"""
+)
+
+for i in range(start_lvl, start_lvl + count):
+    f.write(
+        f"#define EPILEPSY_PRIV_REC_{i}(choice, ...) EPILEPSY_PRIV_REC_NEXT({i + 1}, choice)(__VA_ARGS__)\n")
+
+f.write(f"\n#endif // {header_guard}\n")
+
+f.close()
+print("Done!")
