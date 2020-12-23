154ac1965a2e8f37bba1cf895415aa6bb2e63119
hirrolot <hirrolot@gmail.com>
2020-12-23 20:35:26 +0600

    Refactor lang.h

--- include/epilepsy/lang.h
+++ include/epilepsy/lang.h
@@ -7,6 +7,7 @@
 #define EPILEPSY_LANG_H
 
 #include <epilepsy/priv/aux.h>
+#include <epilepsy/priv/variadics/get.h>
 
 // Desugaring {
 #define call(op, ...) EPILEPSY_PRIV_call(op, __VA_ARGS__)
@@ -16,19 +17,15 @@
 // Implementation {
 #define EPILEPSY_PRIV_call(op, ...)                                                                \
     EPILEPSY_PRIV_IF(                                                                              \
-        EPILEPSY_PRIV_LANG_IS_TRIVIAL_OP(op), EPILEPSY_PRIV_call_TRIVIAL,                          \
-        EPILEPSY_PRIV_call_NON_TRIVIAL)                                                            \
+        EPILEPSY_PRIV_LANG_IS_TRIVIAL_OP(op), EPILEPSY_PRIV_call_0args, EPILEPSY_PRIV_call_0op)    \
     (op, __VA_ARGS__),
 
-#define EPILEPSY_PRIV_call_TRIVIAL(ident, ...)              (0args, ident, __VA_ARGS__)
-#define EPILEPSY_PRIV_call_NON_TRIVIAL(op, _emptiness, ...) (0op, op, __VA_ARGS__)
+#define EPILEPSY_PRIV_call_0args(ident, ...)        (0args, ident, __VA_ARGS__)
+#define EPILEPSY_PRIV_call_0op(op, _emptiness, ...) (0op, op, __VA_ARGS__)
 
 #define EPILEPSY_PRIV_LANG_IS_TRIVIAL_OP(op)                                                       \
-    EPILEPSY_PRIV_LANG_IS_TRIVIAL_OP_AUX(EPILEPSY_PRIV_LANG_IS_TRIVIAL_OP_NO op, 1, ~)
-#define EPILEPSY_PRIV_LANG_IS_TRIVIAL_OP_AUX(...)                                                  \
-    EPILEPSY_PRIV_LANG_IS_TRIVIAL_OP_AUX_AUX(__VA_ARGS__)
-#define EPILEPSY_PRIV_LANG_IS_TRIVIAL_OP_AUX_AUX(x, y, ...) y
-#define EPILEPSY_PRIV_LANG_IS_TRIVIAL_OP_NO(...)            ~, 0
+    EPILEPSY_PRIV_VARIADICS_SND(EPILEPSY_PRIV_LANG_IS_TRIVIAL_OP_TEST op, 1, ~)
+#define EPILEPSY_PRIV_LANG_IS_TRIVIAL_OP_TEST(...) ~, 0
 
 #define EPILEPSY_PRIV_v(...) (0v, __VA_ARGS__),
 // }
