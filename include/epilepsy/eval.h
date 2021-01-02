e72c8cffe2e8314afc2d4b2630410be14ccefc55
hirrolot <hirrolot@gmail.com>
2021-01-02 23:59:40 +0600

    Shorten the names in eval/term.h

--- include/epilepsy/eval.h
+++ include/epilepsy/eval.h
@@ -17,11 +17,11 @@
 #define EPILEPSY_EVAL(...)                                                                         \
     EPILEPSY_PRIV_REC_UNROLL(EPILEPSY_PRIV_EVAL_MATCH(                                             \
         EPILEPSY_PRIV_REC_STOP, (~), EPILEPSY_PRIV_EVAL_LFOLDER_APPEND,                            \
-        EPILEPSY_PRIV_EVAL_ACC_EMPTY(), __VA_ARGS__ EPILEPSY_PRIV_EVAL_TERM_END(), ~))
+        EPILEPSY_PRIV_EVAL_ACC_EMPTY(), __VA_ARGS__ EPILEPSY_PRIV_TERM_END(), ~))
 
 #define EPILEPSY_PRIV_EVAL_MATCH(...) EPILEPSY_PRIV_EVAL_MATCH_AUX(__VA_ARGS__)
 #define EPILEPSY_PRIV_EVAL_MATCH_AUX(k, k_cx, lfolder, acc, head, ...)                             \
-    EPILEPSY_PRIV_EVAL_TERM_MATCH(                                                                 \
+    EPILEPSY_PRIV_TERM_MATCH(                                                                      \
         EPILEPSY_PRIV_EVAL_, head, k, k_cx, lfolder, acc, EPILEPSY_PRIV_EVAL_CONTROL(__VA_ARGS__))
 
 // Reduction rules {
@@ -33,13 +33,13 @@
     EPILEPSY_PRIV_EVAL_MACHINE_REDUCE(                                                             \
         EPILEPSY_PRIV_EVAL_0args_K_HOOK, (k, k_cx, lfolder, acc, tail, op),                        \
         EPILEPSY_PRIV_EVAL_LFOLDER_COMMA, EPILEPSY_PRIV_EVAL_ACC_EMPTY(),                          \
-        __VA_ARGS__ EPILEPSY_PRIV_EVAL_TERM_END(), ~)
+        __VA_ARGS__ EPILEPSY_PRIV_TERM_END(), ~)
 
 #define EPILEPSY_PRIV_EVAL_0op(k, k_cx, lfolder, acc, tail, op, ...)                               \
     EPILEPSY_PRIV_EVAL_MACHINE_REDUCE(                                                             \
         EPILEPSY_PRIV_EVAL_0op_K_HOOK, (k, k_cx, lfolder, acc, tail, (__VA_ARGS__)),               \
         EPILEPSY_PRIV_EVAL_LFOLDER_APPEND, EPILEPSY_PRIV_EVAL_ACC_EMPTY(), op,                     \
-        EPILEPSY_PRIV_EVAL_TERM_END(), ~)
+        EPILEPSY_PRIV_TERM_END(), ~)
 
 #define EPILEPSY_PRIV_EVAL_0end(k, k_cx, _lfolder, acc, _tail, _)                                  \
     EPILEPSY_PRIV_EVAL_MACHINE_CALL_K(k, k_cx, acc)
--- include/epilepsy/eval/term.h
+++ include/epilepsy/eval/term.h
@@ -4,19 +4,19 @@
 #include <epilepsy/priv/aux.h>
 #include <epilepsy/priv/variadics/get.h>
 
-#define EPILEPSY_PRIV_EVAL_TERM_END() (0end, ~)
+#define EPILEPSY_PRIV_TERM_END() (0end, ~)
 
-#define EPILEPSY_PRIV_EVAL_TERM_KIND(term)                                                         \
+#define EPILEPSY_PRIV_TERM_KIND(term)                                                              \
     EPILEPSY_PRIV_VARIADICS_HEAD(EPILEPSY_PRIV_UNPARENTHESISE(term))
 
-#define EPILEPSY_PRIV_EVAL_TERM_DATA(term)                                                         \
+#define EPILEPSY_PRIV_TERM_DATA(term)                                                              \
     EPILEPSY_PRIV_VARIADICS_TAIL(EPILEPSY_PRIV_UNPARENTHESISE(term))
 
-#define EPILEPSY_PRIV_EVAL_TERM_MATCH(f, term, ...)                                                \
-    EPILEPSY_PRIV_EVAL_TERM_MATCH_AUX(                                                             \
-        EPILEPSY_PRIV_MATCH(f, EPILEPSY_PRIV_EVAL_TERM_KIND(term)), __VA_ARGS__,                   \
-        EPILEPSY_PRIV_EVAL_TERM_DATA(term))
+#define EPILEPSY_PRIV_TERM_MATCH(f, term, ...)                                                     \
+    EPILEPSY_PRIV_TERM_MATCH_AUX(                                                                  \
+        EPILEPSY_PRIV_MATCH(f, EPILEPSY_PRIV_TERM_KIND(term)), __VA_ARGS__,                        \
+        EPILEPSY_PRIV_TERM_DATA(term))
 
-#define EPILEPSY_PRIV_EVAL_TERM_MATCH_AUX(f, ...) f(__VA_ARGS__)
+#define EPILEPSY_PRIV_TERM_MATCH_AUX(f, ...) f(__VA_ARGS__)
 
 #endif // EPILEPSY_EVAL_TERM_H
