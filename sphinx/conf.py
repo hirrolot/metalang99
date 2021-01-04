51792e8cc28f2c28dafa5d9079b0fdc86511ea24
hirrolot <hirrolot@gmail.com>
2021-01-04 09:24:24 +0600

    Remove sphinx/build/

--- sphinx/conf.py
+++ sphinx/conf.py
@@ -48,12 +48,6 @@ exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 #
 html_theme = 'nature'
 
-# Add any paths that contain custom static files (such as style sheets) here,
-# relative to this directory. They are copied after the builtin static files,
-# so a file named "default.css" will overwrite the builtin "default.css".
-html_static_path = ['_static']
-
-
 breathe_projects = {"Epilepsy": "../xml"}
 breathe_default_project = "Epilepsy"
 primary_domain = 'c'
