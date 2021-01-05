d71befe94730c94d26f3728dd18954c6bf21298b
hirrolot <hirrolot@gmail.com>
2021-01-06 02:33:55 +0600

    Stringify an error message

--- include/epilepsy/eval.h
+++ include/epilepsy/eval.h
@@ -58,7 +58,7 @@
 
 // clang-format off
 #define EPILEPSY_PRIV_EVAL_0error(_k, _k_cx, _lfolder, _acc, _tail, f, ...)                        \
-    EPILEPSY_PRIV_REC_CONTINUE(EPILEPSY_PRIV_REC_STOP, (~), !Epilepsy error (f): __VA_ARGS__)
+    EPILEPSY_PRIV_REC_CONTINUE(EPILEPSY_PRIV_REC_STOP, (~), !Epilepsy error (f): #__VA_ARGS__)
 // clang-format on
 
 #define EPILEPSY_PRIV_EVAL_0dbg(_k, _k_cx, _lfolder, _acc, _tail, ...)                             \
