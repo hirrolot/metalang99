a7466783343a9c2eeb6053259f87ed6423a08460
hirrolot <hirrolot@gmail.com>
2021-01-13 22:52:02 +0600

    Complete tests/variadics/get.h

--- tests/variadics/get.h
+++ tests/variadics/get.h
@@ -1,15 +1,21 @@
 #include <epilepsy/assert.h>
 #include <epilepsy/variadics.h>
 
-E_assertEq(E_variadicsHead(v(51) v(21) v(1) v(7378)), v(51));
-E_assertEq(E_variadicsHead(v(51) v(21) v(1) v(7378)), v(51));
+// E_variadicsHead {
+E_assertEq(E_variadicsHead(v(19, 8)), v(19));
+E_assertEq(E_variadicsHead(v(19, 8, 7378)), v(19));
+// }
 
-#define CHECK_TAIL_IMPL(a, b, c, d)                                                                \
-    v(E_assertPlain(a == 51, ""); E_assertPlain(b == 21, ""); E_assertPlain(c == 1, "");           \
-      E_assertPlain(d == 7378, "");)
+// E_variadicsTail {
+#define CHECK_TAIL(...) CHECK_TAIL_AUX(__VA_ARGS__)
+#define CHECK_TAIL_AUX(a, b, c, d)                                                                 \
+    E_assertPlain(a == 51);                                                                        \
+    E_assertPlain(b == 21);                                                                        \
+    E_assertPlain(c == 1);                                                                         \
+    E_assertPlain(d == 7378)
 
-E_assertEq(E_variadicsTail(v(120) v(19801)), v(19801));
-// E_eval(E_call(CHECK_TAIL, E_call(E_variadicsTail, v(9191) v(51)
-// v(21) v(7378))))
+CHECK_TAIL(E_eval(E_variadicsTail(v(9191, 51, 21, 1, 7378))));
 
-#undef CHECK_TAIL_IMPL
+#undef CHECK_TAIL
+#undef CHECK_TAIL_AUX
+// }
