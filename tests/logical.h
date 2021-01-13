a40b410485f958ff3f5e071df2a3991a57dca5f4
hirrolot <hirrolot@gmail.com>
2021-01-13 22:55:12 +0600

    Use the same commenting conventions in tests/logical.h

--- tests/logical.h
+++ tests/logical.h
@@ -1,20 +1,28 @@
 #include <epilepsy/assert.h>
 #include <epilepsy/logical.h>
 
+// E_not {
 E_assertEq(E_not(v(0)), v(1));
 E_assertEq(E_not(v(1)), v(0));
+// }
 
+// E_and {
 E_assertEq(E_and(v(0), v(0)), v(0));
 E_assertEq(E_and(v(0), v(1)), v(0));
 E_assertEq(E_and(v(1), v(0)), v(0));
 E_assertEq(E_and(v(1), v(1)), v(1));
+// }
 
+// E_or {
 E_assertEq(E_or(v(0), v(0)), v(0));
 E_assertEq(E_or(v(0), v(1)), v(1));
 E_assertEq(E_or(v(1), v(0)), v(1));
 E_assertEq(E_or(v(1), v(1)), v(1));
+// }
 
+// E_xor {
 E_assertEq(E_xor(v(0), v(0)), v(0));
 E_assertEq(E_xor(v(0), v(1)), v(1));
 E_assertEq(E_xor(v(1), v(0)), v(1));
 E_assertEq(E_xor(v(1), v(1)), v(0));
+// }
