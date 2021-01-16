4dfad9c3c3102bbcab87ed64d5788369964d8601
hirrolot <hirrolot@gmail.com>
2021-01-16 18:59:16 +0600

    Change the diagnostics format

--- include/epilepsy/eval/diagnostics.h
+++ include/epilepsy/eval/diagnostics.h
@@ -5,7 +5,7 @@
 
 // clang-format off
 #define EPILEPSY_PRIV_DIAGNOSTICS_SYNTAX_ERROR(invalid_term)                                       \
-    EPILEPSY_PRIV_REC_CONTINUE(EPILEPSY_PRIV_REC_STOP, (~), !Epilepsy syntax error: {invalid_term} is not a valid term)
+    EPILEPSY_PRIV_REC_CONTINUE(EPILEPSY_PRIV_REC_STOP, (~), !Epilepsy syntax error: `invalid_term` is not a valid term)
 // clang-format on
 
 #endif // EPILEPSY_EVAL_DIAGNOSTICS_H
