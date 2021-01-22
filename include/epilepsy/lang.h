1af2a198be44a9741da748e5a1179fae23e1f28c
hirrolot <hirrolot@gmail.com>
2021-01-22 15:26:01 +0600

    Make more elaborate documentation for partial application

--- include/epilepsy/lang.h
+++ include/epilepsy/lang.h
@@ -18,16 +18,91 @@
 
 /**
  * Applies arguments to @p f.
+ *
+ * This function implements <a href="https://en.wikipedia.org/wiki/Partial_application">partial
+ * application</a>: instead of invoking a metafunction with all arguments at once, you specify each
+ * argument separately. This concept allows to re-use existing metafunctions by specifying some
+ * arguments immediately, and the other arguments later, even in different execution contexts (for
+ * example, see this <a href="https://stackoverflow.com/a/12414292/13166656">SO answer</a>).
+ *
+ * @p f shall be either a term reducing to a macro name or a term obtained via another call to
+ * #EPILEPSY_appl. If @p f is a macro name, then `<f>_ARITY` shall denote the arity of @p f (e.g.,
+ * the number of parameters it accepts), by the following rules:
+ *
+ *  - A metafunction with no parameters has the arity 1 (because `E_appl(v(F), v())` shall has the
+ * meaning of an application of the single empty argument `v()`).
+ *  - A single named parameter, as well as an ellipsis denoting variadic arguments, increases the
+ * arity of a metafunction by 1.
+ *
+ * These are some examples of correspondence between the macro signatures and their arities:
+ *
+ * @code
+ * #define F() // ...
+ * #define F_ARITY 1
+ *
+ * #define F(a, b, c) // ...
+ * #define F_ARITY 3
+ *
+ * #define F(...) // ...
+ * #define F_ARITY 1
+ *
+ * #define F(a, b, c, ...) // ...
+ * #define F_ARITY 4
+ * @endcode
+ *
+ * Each time except the last #EPILEPSY_appl returns a term that can be passed further to
+ * #EPILEPSY_appl to specify the next argument; when the last argument has been specified, the
+ * initial metafunction is invoked with all the accumulated arguments. If a metafunction under
+ * partial application is variadic, then all variadic arguments shall be specified at once.
+ *
+ * # Examples
+ *
+ * @code
+ * #include <epilepsy/lang.h>
+ * #include <epilepsy/eval.h>
+ *
+ * #define F_IMPL(x, y) v(x##y)
+ * #define F_ARITY      2
+ *
+ * // ab
+ * E_appl(E_appl(v(F), v(a)), v(b))
+ * @endcode
  */
 #define EPILEPSY_appl(f, ...) EPILEPSY_call(EPILEPSY_appl, f __VA_ARGS__)
 
 /**
  * Applies @p a and @p b to @p f.
+ *
+ * # Examples
+ *
+ * @code
+ * #include <epilepsy/lang.h>
+ * #include <epilepsy/eval.h>
+ *
+ * #define F_IMPL(x, y) v(x##y)
+ * #define F_ARITY      2
+ *
+ * // ab
+ * E_appl2(v(F), v(a), v(b))
+ * @endcode
  */
 #define EPILEPSY_appl2(f, a, b) EPILEPSY_call(EPILEPSY_appl2, f a b)
 
 /**
  * Applies @p a, @p b, and @p c @p f.
+ *
+ * # Examples
+ *
+ * @code
+ * #include <epilepsy/lang.h>
+ * #include <epilepsy/eval.h>
+ *
+ * #define F_IMPL(x, y, z) v(x##y##z)
+ * #define F_ARITY         3
+ *
+ * // abc
+ * E_appl3(v(F), v(a), v(b), v(c))
+ * @endcode
  */
 #define EPILEPSY_appl3(f, a, b, c) EPILEPSY_call(EPILEPSY_appl3, f a b c)
 
