b7101b8ef2ba65996eb1df3cbaf9ef1bb5c6f4f0
hirrolot <hirrolot@gmail.com>
2021-01-22 11:29:09 +0600

    Refactor tests/overload.h

--- tests/overload.h
+++ tests/overload.h
@@ -4,13 +4,9 @@
 // E_overload {
 #define X_IMPL(...)    E_overload(v(X_), v(__VA_ARGS__))
 #define X_1_IMPL(a)    v(E_assertPlain(a == 123))
-#define X_2_IMPL(a, b) v(E_assertPlain(a == 93145); E_assertPlain(b == 456))
+#define X_2_IMPL(a, b) v(E_assertPlain(a == 93145 && b == 456))
 #define X_7_IMPL(a, b, c, d, e, f, g)                                                              \
-    v(E_assertPlain(a == 1516); E_assertPlain(b == 1); E_assertPlain(c == 9);                      \
-      E_assertPlain(d == 111);                                                                     \
-      E_assertPlain(e == 119);                                                                     \
-      E_assertPlain(f == 677);                                                                     \
-      E_assertPlain(g == 62))
+    v(E_assertPlain(a == 1516 && b == 1 && c == 9 && d == 111 && e == 119 && f == 677 && g == 62))
 
 E_eval(E_call(X, v(123)));
 E_eval(E_call(X, v(93145, 456)));
