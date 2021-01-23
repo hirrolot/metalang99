cb0573bbcfe4f62cdef4dc32ae18df50c7c612d1
hirrolot <hirrolot@gmail.com>
2021-01-23 23:45:09 +0600

    Rework examples/demo.c

--- examples/demo.c
+++ examples/demo.c
@@ -1,38 +1,40 @@
 #include <epilepsy.h>
 
-int main(void) {
-    // Compile-time list manipulation {
-    // "Rachmaninoff" "Bach" "Chopin"
-    E_listEval(E_listReverse(E_list(v("Chopin", "Bach", "Rachmaninoff"))));
+// Compile-time list manipulation {
+// puts("c");
+// puts("b");
+// puts("a");
+E_listEval(E_listReverse(E_list(v(puts("a");, puts("b");, puts("c");))));
 
-    // 9 2 5
-    E_listEval(E_listFilter(E_list(v(9, 2, 11, 13, 5)), E_appl(v(E_uintGreater), v(10))));
+// 9 2 5
+E_listEval(E_listFilter(E_list(v(9, 2, 11, 13, 5)), E_appl(v(E_uintGreater), v(10))));
 
-    // 4 5 6
-    E_listEval(E_listMap(E_appl(v(E_uintAdd), v(3)), E_list(v(1, 2, 3))));
-    // }
+// 4 5 6
+E_listEval(E_listMap(E_appl(v(E_uintAdd), v(3)), E_list(v(1, 2, 3))));
+// }
 
-    // General macro recursion {
+// General macro recursion {
 #define factorial(n) E_call(factorial, n)
 
 #define factorial_IMPL(n)   E_uintMatch(v(n), v(factorial_))
 #define factorial_Z_IMPL()  v(1)
 #define factorial_S_IMPL(n) E_uintMul(E_uintInc(v(n)), factorial(v(n)))
 
-    static int _24 = E_eval(factorial(v(4)));
-    // }
+E_assertEq(factorial(v(4)), v(24));
+// }
 
-    // Overloading on a number of arguments {
-    typedef struct {
-        double width, height;
-    } Rect;
+// Overloading on a number of arguments {
+typedef struct {
+    double width, height;
+} Rect;
 
 #define Rect_new(...)         E_eval(E_overload(v(Rect_new_), v(__VA_ARGS__)))
 #define Rect_new_1_IMPL(x)    v(((Rect){.width = x, .height = x}))
 #define Rect_new_2_IMPL(x, y) v(((Rect){.width = x, .height = y}))
 
-    static Rect _7x8 = Rect_new(7, 8), _10x10 = Rect_new(10);
-    // }
+static Rect _7x8 = Rect_new(7, 8), _10x10 = Rect_new(10);
+// }
 
-    // ... and more!
-}
+// ... and more!
+
+int main(void) {}
