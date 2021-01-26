8217aa03c571eead6c554bf774547d5174fa6cf7
hirrolot <hirrolot@gmail.com>
2021-01-26 14:25:27 +0600

    Refactor examples/match_string.c

--- examples/match_string.c
+++ examples/match_string.c
@@ -13,20 +13,18 @@
 
 #define MATCH(str, ...)                                                                            \
     {                                                                                              \
-        const char *matched_str = (const char *)(str);                                             \
-                                                                                                   \
         E_listEval(E_listMap(                                                                      \
-            E_compose(v(GEN_BRANCH), v(E_unparenthesiseUnevaluated)),                              \
+            E_compose(E_appl(v(GEN_BRANCH), v(str)), v(E_unparenthesiseUnevaluated)),              \
             E_list(v(__VA_ARGS__))))                                                               \
                                                                                                    \
             out:;                                                                                  \
     }
 
-#define GEN_BRANCH_IMPL(...) E_call(GEN_BRANCH_AUX, v(__VA_ARGS__))
-#define GEN_BRANCH_ARITY     1
+#define GEN_BRANCH_IMPL(str, ...) E_call(GEN_BRANCH_AUX, v(str) v(__VA_ARGS__))
+#define GEN_BRANCH_AUX_IMPL(str, branch_str, ...)                                                  \
+    v(if (strcmp(str, branch_str) == 0) { __VA_ARGS__ goto out; })
 
-#define GEN_BRANCH_AUX_IMPL(branch_str, ...)                                                       \
-    v(if (strcmp(matched_str, branch_str) == 0) { __VA_ARGS__ goto out; })
+#define GEN_BRANCH_ARITY 2
 
 int main(void) {
     const char *reason = "OK";
