cd2b733fcc4184b5b3c147fd6c79c3a3cdd9a579
hirrolot <hirrolot@gmail.com>
2021-01-26 15:51:53 +0600

    Implement syntax-directed translation in examples/match_string.c

--- examples/match_string.c
+++ examples/match_string.c
@@ -1,5 +1,5 @@
 /*
- * Pattern matching for strings (like a switch statement).
+ * Pattern matching for strings using syntax-directed translation.
  *
  * We could accomplish the same behaviour with X-macro, but it would require additional hassle with
  * #define and #undef directives.
@@ -11,25 +11,62 @@
 
 #include <epilepsy.h>
 
-#define MATCH(str, ...)                                                                            \
-    {                                                                                              \
-        E_listEval(E_listMap(                                                                      \
-            E_compose(E_appl(v(GEN_BRANCH), v(str)), v(E_unparenthesiseUnevaluated)),              \
-            E_listInit(E_list(v(__VA_ARGS__)))))                                                   \
-                                                                                                   \
-            GEN_DEFAULT_CASE(__VA_ARGS__)                                                          \
-                                                                                                   \
-                out:;                                                                              \
-    }
+#define MATCH(val, ...)                                                                            \
+    { E_eval(matchStmtGen(matchStmtParse(v(val), v(__VA_ARGS__)))) out:; }
 
-#define GEN_BRANCH_IMPL(str, ...) E_call(GEN_BRANCH_AUX, v(str) v(__VA_ARGS__))
-#define GEN_BRANCH_AUX_IMPL(str, pattern, ...)                                                     \
-    v(if (strcmp(str, pattern) == 0) { __VA_ARGS__ goto out; })
+#define matchStmt(val, branches, default_case) E_call(matchStmt, val branches default_case)
+#define matchStmtVal                           v(0)
+#define matchStmtBranches                      v(1)
+#define matchStmtDefaultCase                   v(2)
 
-#define GEN_BRANCH_ARITY 2
+#define branch(pattern, ...) E_call(branch, pattern __VA_ARGS__)
+#define branchPattern        v(0)
+#define branchBody           v(1)
 
-#define GEN_DEFAULT_CASE(...)                                                                      \
-    E_eval(E_unparenthesiseUnevaluated(E_listLast(E_list(v(__VA_ARGS__)))))
+#define defaultCase(...) E_call(defaultCase, __VA_ARGS__)
+#define defaultCaseBody  v(0)
+
+#define matchStmtParse(val, ...) E_call(matchStmtParse, val __VA_ARGS__)
+#define branchParse(x)           E_call(branchParse, x)
+#define branchParseAux(...)      E_call(branchParseAux, __VA_ARGS__)
+#define defaultCaseParse(...)    E_call(defaultCaseParse, __VA_ARGS__)
+
+#define matchStmtGen(match_stmt)     E_call(matchStmtGen, match_stmt)
+#define branchGen(val, branch)       E_call(branchGen, val branch)
+#define defaultCaseGen(default_case) E_call(defaultCaseGen, default_case)
+
+#define matchStmt_IMPL(val, branches, default_case) E_list(v(val) v(branches) v(default_case))
+#define branch_IMPL(pattern, ...)                   E_list(v(pattern) E_parenthesiseUnevaluated(v(__VA_ARGS__)))
+#define defaultCase_IMPL(...)                       E_list(E_parenthesiseUnevaluated(v(__VA_ARGS__)))
+
+#define matchStmtParse_IMPL(val, ...)                                                              \
+    matchStmt(                                                                                     \
+        v(val),                                                                                    \
+        E_listMap(v(branchParse), E_listInit(E_list(v(__VA_ARGS__)))),                             \
+        defaultCaseParse(v(__VA_ARGS__)))
+#define branchParse_IMPL(x) branchParseAux(E_unparenthesiseUnevaluated(v(x)))
+#define branchParseAux_IMPL(...)                                                                   \
+    branch(E_variadicsHead(v(__VA_ARGS__)), E_variadicsTail(v(__VA_ARGS__)))
+#define defaultCaseParse_IMPL(...)                                                                 \
+    defaultCase(E_unparenthesiseUnevaluated(E_listLast(E_list(v(__VA_ARGS__)))))
+
+#define matchStmtGen_IMPL(match_stmt)                                                              \
+    E_listUnwrap(E_listMap(                                                                        \
+        E_appl(v(branchGen), E_get(matchStmtVal, v(match_stmt))),                                  \
+        E_get(matchStmtBranches, v(match_stmt))))                                                  \
+        defaultCaseGen(E_get(matchStmtDefaultCase, v(match_stmt)))
+#define branchGen_IMPL(val, branch)                                                                \
+    E_call(                                                                                        \
+        branchGenAux,                                                                              \
+        v(val) E_get(branchPattern, v(branch))                                                     \
+            E_unparenthesiseUnevaluated(E_get(branchBody, v(branch))))
+#define branchGenAux_IMPL(val, pattern, ...)                                                       \
+    v(if (strcmp(val, pattern) == 0) { __VA_ARGS__ goto out; })
+#define defaultCaseGen_IMPL(default_case)                                                          \
+    E_unparenthesiseUnevaluated(E_get(defaultCaseBody, v(default_case)))
+
+#define branchGen_ARITY   2
+#define branchParse_ARITY 1
 
 int main(void) {
     const char *reason = "OK";
