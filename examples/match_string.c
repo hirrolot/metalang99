7af22449c489d7b1ab79705bf40ea454e0352503
hirrolot <hirrolot@gmail.com>
2021-01-27 19:35:32 +0600

    Use E_variadicsMatch in examples/match_string.c

--- examples/match_string.c
+++ examples/match_string.c
@@ -1,11 +1,5 @@
-/*
- * Pattern matching for strings (like a switch statement).
- *
- * We could accomplish the same behaviour with X-macro, but it would require additional hassle with
- * #define and #undef directives.
- */
+// Pattern matching for strings (like a switch statement).
 
-#include <stdbool.h>
 #include <stdio.h>
 #include <string.h>
 
@@ -14,18 +8,19 @@
 #define MATCH(str, ...)                                                                            \
     {                                                                                              \
         E_listEval(E_listMapInitLast(                                                              \
-            E_compose(E_appl(v(GEN_BRANCH), v(str)), v(E_unparenthesise)),                         \
+            E_compose(                                                                             \
+                E_appl(v(E_variadicsMatch), E_appl(v(GEN_BRANCH), v(str))),                        \
+                v(E_unparenthesise)),                                                              \
             v(GEN_DEFAULT_BRANCH),                                                                 \
             E_list(v(__VA_ARGS__)))) out:;                                                         \
     }
 
-#define GEN_BRANCH_IMPL(str, ...) E_call(GEN_BRANCH_AUX, v(str, __VA_ARGS__))
-#define GEN_BRANCH_AUX_IMPL(str, pattern, ...)                                                     \
+#define GEN_BRANCH_IMPL(str, pattern, ...)                                                         \
     v(if (strcmp(str, pattern) == 0) { __VA_ARGS__ goto out; })
 
-#define GEN_BRANCH_ARITY 2
-
 #define GEN_DEFAULT_BRANCH E_unparenthesise
+
+#define GEN_BRANCH_ARITY 3
 
 int main(void) {
     const char *reason = "OK";
