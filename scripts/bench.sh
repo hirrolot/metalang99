5e74fc527a8c4f06b9a3dac6446cdc6968516da5
hirrolot <hirrolot@gmail.com>
2021-02-01 21:08:37 +0600

    Add bench/list_of_100_items.h

--- scripts/bench.sh
+++ scripts/bench.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
 
+echo "compare_25_items.h"
 time gcc bench/compare_25_items.h -ftrack-macro-expansion=0 -Iinclude
+
+echo ""
+echo "list_of_100_items.h"
+time gcc bench/list_of_100_items.h -ftrack-macro-expansion=0 -Iinclude
