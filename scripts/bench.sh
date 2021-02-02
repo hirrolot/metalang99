753893f7ef104157a89cc1b827bbb25cd201dbb7
hirrolot <hirrolot@gmail.com>
2021-02-02 17:58:37 +0600

    Add bench/100_call.h, 100_v.h

--- scripts/bench.sh
+++ scripts/bench.sh
@@ -6,3 +6,11 @@ time gcc bench/compare_25_items.h -ftrack-macro-expansion=0 -Iinclude
 echo ""
 echo "list_of_100_items.h"
 time gcc bench/list_of_100_items.h -ftrack-macro-expansion=0 -Iinclude
+
+echo ""
+echo "100_v.h"
+time gcc bench/100_v.h -ftrack-macro-expansion=0 -Iinclude
+
+echo ""
+echo "100_call.h"
+time gcc bench/100_call.h -ftrack-macro-expansion=0 -Iinclude
