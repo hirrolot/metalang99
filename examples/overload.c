45bbc1306ad9f9d24817e6eeb000ee9a94105fa0
hirrolot <hirrolot@gmail.com>
2021-02-08 21:41:28 +0600

    Fix examples/overload.c

--- examples/overload.c
+++ examples/overload.c
@@ -5,9 +5,11 @@ typedef struct {
     double width, height;
 } Rect;
 
-#define Rect_new(...)    M_overloadPlain(Rect_new_, __VA_ARGS__)
-#define Rect_new_1(x)    ((Rect){.width = x, .height = x})
-#define Rect_new_2(x, y) ((Rect){.width = x, .height = y})
+#define Rect_new(...) M_overloadPlain(Rect_new_, __VA_ARGS__)
+#define Rect_new_1(x)                                                                              \
+    { x, x }
+#define Rect_new_2(x, y)                                                                           \
+    { x, y }
 
 static Rect _7x8 = Rect_new(7, 8), _10x10 = Rect_new(10);
 
