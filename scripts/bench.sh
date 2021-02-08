5a718569a5f13f17e1fa947767b259620aa6d207
hirrolot <hirrolot@gmail.com>
2021-02-08 22:01:37 +0600

    bench/list_of_100_items.h -> list_of_63_items.h

--- scripts/bench.sh
+++ scripts/bench.sh
@@ -4,8 +4,8 @@ echo "compare_25_items.h"
 time gcc bench/compare_25_items.h -ftrack-macro-expansion=0 -Iinclude
 
 echo ""
-echo "list_of_100_items.h"
-time gcc bench/list_of_100_items.h -ftrack-macro-expansion=0 -Iinclude
+echo "list_of_63_items.h"
+time gcc bench/list_of_63_items.h -ftrack-macro-expansion=0 -Iinclude
 
 echo ""
 echo "100_v.h"
