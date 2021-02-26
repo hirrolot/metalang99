dcbedbc52e56a234bcf3a5919ab0398de43566cb
hirrolot <hirrolot@gmail.com>
2021-02-26 17:51:10 +0600

    Simplify examples/binary_tree.c

--- examples/binary_tree.c
+++ examples/binary_tree.c
@@ -2,17 +2,12 @@
 
 #include <metalang99.h>
 
-#define TreeLeaf(x)              M_call(TreeLeaf, x)
-#define TreeNode(lhs, data, rhs) M_call(TreeNode, lhs, data, rhs)
+#define Leaf(x)              M_choice(v(Leaf), x)
+#define Node(lhs, data, rhs) M_choice(v(Node), lhs, data, rhs)
 
-#define SUM(tree) M_call(SUM, tree)
-
-#define TreeLeaf_IMPL(x)              M_choice(v(TreeLeaf), v(x))
-#define TreeNode_IMPL(lhs, data, rhs) M_choice(v(TreeNode), v(lhs, data, rhs))
-
-#define SUM_IMPL(tree)                    M_match(v(tree), v(SUM_))
-#define SUM_TreeLeaf_IMPL(x)              v(x)
-#define SUM_TreeNode_IMPL(lhs, data, rhs) M_add3(SUM(v(lhs)), v(data), SUM(v(rhs)))
+#define SUM(tree)                     M_match(tree, v(SUM_))
+#define SUM_Leaf_IMPL(x)              v(x)
+#define SUM_Node_IMPL(lhs, data, rhs) M_add3(SUM(v(lhs)), v(data), SUM(v(rhs)))
 
 /*
  *         4
@@ -23,11 +18,7 @@
  *    / \     / \
  *   1   3   5   7
  */
-#define TREE                                                                                       \
-    TreeNode(                                                                                      \
-        TreeNode(TreeLeaf(v(1)), v(2), TreeLeaf(v(3))),                                            \
-        v(4),                                                                                      \
-        TreeNode(TreeLeaf(v(5)), v(6), TreeLeaf(v(7))))
+#define TREE Node(Node(Leaf(v(1)), v(2), Leaf(v(3))), v(4), Node(Leaf(v(5)), v(6), Leaf(v(7))))
 
 M_assertEq(SUM(TREE), v(28));
 
