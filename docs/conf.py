99d0e26048fd86c541006ea029f55c6c85025c1d
hirrolot <hirrolot@gmail.com>
2021-02-26 18:10:29 +0600

    Prepare for v0.4.0

--- docs/conf.py
+++ docs/conf.py
@@ -25,7 +25,7 @@ copyright = '2021, Hirrolot'
 author = 'Hirrolot'
 
 # The full version, including alpha/beta/rc tags
-release = '0.3.0'
+release = '0.4.0'
 
 
 # -- General configuration ---------------------------------------------------
