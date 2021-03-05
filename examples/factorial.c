469f56ad4eaec0b2b94c067861df33aba4d1b6ec
hirrolot <hirrolot@gmail.com>
2021-03-05 23:39:06 +0600

    Simplify examples/factorial.c, demo.c

--- examples/factorial.c
+++ examples/factorial.c
@@ -1,8 +1,6 @@
 #include <metalang99.h>
 
-#define factorial(n) M_call(factorial, n)
-
-#define factorial_IMPL(n)   M_natMatch(v(n), v(factorial_))
+#define factorial(n)        M_natMatch(n, v(factorial_))
 #define factorial_Z_IMPL()  v(1)
 #define factorial_S_IMPL(n) M_mul(M_inc(v(n)), factorial(v(n)))
 
