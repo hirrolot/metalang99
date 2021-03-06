3280992427209882601d527c37538f9ba52bc259
hirrolot <hirrolot@gmail.com>
2021-03-06 22:14:12 +0600

    Simplify examples/ackermann.c

--- examples/ackermann.c
+++ examples/ackermann.c
@@ -1,11 +1,9 @@
 #include <metalang99.h>
 
-#define ack(m, n) M_call(ack, m, n)
-
-#define ack_IMPL(m, n)   M_natMatchWithArgs(v(m), v(ack_), v(n))
-#define ack_S_IMPL(m, n) M_natMatchWithArgs(v(n), v(ack_S_), v(m))
+#define ack(m, n) M_natMatchWithArgs(m, v(ack_), n)
 
 #define ack_Z_IMPL(n)      M_inc(v(n))
+#define ack_S_IMPL(m, n)   M_natMatchWithArgs(v(n), v(ack_S_), v(m))
 #define ack_S_Z_IMPL(m)    ack(v(m), v(1))
 #define ack_S_S_IMPL(n, m) ack(v(m), ack(M_inc(v(m)), v(n)))
 
