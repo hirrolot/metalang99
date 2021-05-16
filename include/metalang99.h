e3e5686cc178bd8550fecf0a9c1502da3e88719f
hirrolot <hirrolot@gmail.com>
2021-05-17 03:32:00 +0600

    Include ident.h in metalang99.h

--- include/metalang99.h
+++ include/metalang99.h
@@ -9,6 +9,7 @@
 #include <metalang99/choice.h>
 #include <metalang99/control.h>
 #include <metalang99/gen.h>
+#include <metalang99/ident.h>
 #include <metalang99/lang.h>
 #include <metalang99/list.h>
 #include <metalang99/maybe.h>
