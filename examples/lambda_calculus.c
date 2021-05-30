d22db20c46c38c9f5d4dd73c078d0675d6456a7f
hirrolot <hirrolot@gmail.com>
2021-05-31 02:51:12 +0600

    Use De Bruijn indices for `examples/lambda_calculus.c`

--- examples/lambda_calculus.c
+++ examples/lambda_calculus.c
@@ -1,60 +1,68 @@
 /*
- * A call-by-name untyped lambda calculus [1] interpreter.
- *
- *  - Only lowercase characters are allowed for bindings (abcdefghijklmnopqrstuvwxyz).
- *  - The Barendregt's convention is employed.
+ * A call-by-name untyped lambda calculus [1] interpreter using De Bruijn indices [2].
  *
  * [1]: https://en.wikipedia.org/wiki/Lambda_calculus
+ * [2]: https://en.wikipedia.org/wiki/De_Bruijn_index
  */
 
 #include <metalang99.h>
 
 // Grammar: Var(iable), Appl(ication), and Lam(bda abstraction) {
-#define Var(x)     ML99_choice(v(Var), x)
+#define Var(i)     ML99_choice(v(Var), i)
 #define Appl(M, N) ML99_choice(v(Appl), M, N)
-#define Lam(x, M)  ML99_choice(v(Lam), x, M)
-
-#define TERM_KIND_EQ(lhs, rhs) ML99_IDENT_EQ(TERM_, lhs, rhs)
-#define TERM_Var_Var           ()
-#define TERM_Appl_Appl         ()
-#define TERM_Lam_Lam           ()
+#define Lam(M)     ML99_choice(v(Lam), M)
 // }
 
 // Substitution: `M[lhs=rhs]` {
-#define subst(M, lhs, rhs)          ML99_matchWithArgs(M, v(subst_), lhs, rhs)
-#define subst_Var_IMPL(x, lhs, rhs) ML99_if(ML99_charEq(v(x), v(lhs)), v(rhs), Var(v(x)))
-#define subst_Appl_IMPL(M, N, lhs, rhs)                                                            \
-    Appl(subst(v(M), v(lhs), v(rhs)), subst(v(N), v(lhs), v(rhs)))
-#define subst_Lam_IMPL(x, M, lhs, rhs)                                                             \
-    ML99_IF(ML99_CHAR_EQ(x, lhs), Lam(v(x), v(M)), Lam(v(x), subst(v(M), v(lhs), v(rhs))))
+#define subst(M, x)                    substAux(M, x, v(1))
+#define substAux(M, x, depth)          ML99_matchWithArgs(M, v(substAux_), x, depth)
+#define substAux_Var_IMPL(i, x, depth) ML99_IF(ML99_NAT_EQ(i, depth), v(x), Var(v(i)))
+#define substAux_Appl_IMPL(M, N, x, depth)                                                         \
+    Appl(substAux(v(M), v(x), v(depth)), substAux(v(N), v(x), v(depth)))
+#define substAux_Lam_IMPL(M, x, depth) Lam(substAux(v(M), incVars(v(x), v(1)), v(ML99_INC(depth))))
+
+#define incVars(M, depth) forEachVar(M, v(ML99_INC), depth)
+#define decVars(M, depth) forEachVar(M, v(ML99_DEC), depth)
+
+// `f` will be applied to all variables >= `depth`.
+#define forEachVar(M, depth, f) ML99_matchWithArgs(M, v(forEachVar_), depth, f)
+#define forEachVar_Var_IMPL(i, depth, f)                                                           \
+    ML99_if(ML99_greaterEq(v(i), v(depth)), Var(v(f(i))), Var(v(i)))
+#define forEachVar_Appl_IMPL(M, N, depth, f)                                                       \
+    Appl(forEachVar(v(M), v(depth), v(f)), forEachVar(v(N), v(depth), v(f)))
+#define forEachVar_Lam_IMPL(M, depth, f) Lam(forEachVar(v(M), v(ML99_INC(depth)), v(f)))
 // }
 
 // Evaluation {
-#define eval(T)              ML99_match(T, v(eval_))
-#define eval_Var_IMPL(x)     Var(v(x))
+#define eval(M)              ML99_match(M, v(eval_))
+#define eval_Var_IMPL(i)     Var(v(i))
 #define eval_Appl_IMPL(M, N) ML99_matchWithArgs(v(M), v(eval_Appl_), v(N))
-#define eval_Lam_IMPL(x, M)  Lam(v(x), v(M))
+#define eval_Lam_IMPL(M)     Lam(v(M))
 
-#define eval_Appl_Var_IMPL(x, N)      Appl(Var(v(x)), v(N))
+#define eval_Appl_Var_IMPL(i, N)      Appl(Var(v(i)), v(N))
 #define eval_Appl_Appl_IMPL(M, N, N1) eval(Appl(eval(Appl(v(M), v(N))), v(N1)))
-#define eval_Appl_Lam_IMPL(x, M, N)   eval(subst(v(M), v(x), v(N)))
+#define eval_Appl_Lam_IMPL(M, N)      eval(subst(decVars(v(M), v(2)), v(N)))
 // }
 
 // Syntactical equality of two terms {
 #define termEq(lhs, rhs)            ML99_matchWithArgs(lhs, v(termEq_), rhs)
-#define termEq_Var_IMPL(x, rhs)     termEqPropagate(Var, rhs, x)
+#define termEq_Var_IMPL(i, rhs)     termEqPropagate(Var, rhs, i)
 #define termEq_Appl_IMPL(M, N, rhs) termEqPropagate(Appl, rhs, M, N)
-#define termEq_Lam_IMPL(x, M, rhs)  termEqPropagate(Lam, rhs, x, M)
+#define termEq_Lam_IMPL(M, rhs)     termEqPropagate(Lam, rhs, M)
 
 #define termEqPropagate(term_kind, rhs, ...)                                                       \
     ML99_IF(                                                                                       \
-        TERM_KIND_EQ(ML99_CHOICE_TAG(rhs), term_kind),                                             \
+        ML99_IDENT_EQ(TERM_, ML99_CHOICE_TAG(rhs), term_kind),                                     \
         ML99_matchWithArgs(v(rhs), v(termEq_##term_kind##_), v(__VA_ARGS__)),                      \
         ML99_false())
 
-#define termEq_Var_Var_IMPL(y, x)           ML99_charEq(v(x), v(y))
+#define termEq_Var_Var_IMPL(j, i)           v(ML99_NAT_EQ(i, j))
 #define termEq_Appl_Appl_IMPL(M, N, M1, N1) ML99_and(termEq(v(M), v(M1)), termEq(v(N), v(N1)))
-#define termEq_Lam_Lam_IMPL(x, M, x1, M1)   ML99_and(ML99_charEq(v(x), v(x1)), termEq(v(M), v(M1)))
+#define termEq_Lam_Lam_IMPL(M, M1)          termEq(v(M), v(M1))
+
+#define TERM_Var_Var   ()
+#define TERM_Appl_Appl ()
+#define TERM_Lam_Lam   ()
 // }
 
 #define ASSERT_REDUCES_TO(lhs, rhs)                                                                \
@@ -63,21 +71,21 @@
     ML99_ASSERT_UNEVAL(ML99_EVAL(termEq(v(ML99_EVAL(eval(lhs))), rhs)))
 
 // The identity combinator {
-#define I Lam(v(a), Var(v(a)))
+#define I Lam(Var(v(1)))
 
-ML99_ASSERT(termEq(eval(Appl(I, Var(v(q)))), Var(v(q))));
+ML99_ASSERT(termEq(eval(Appl(I, Var(v(5)))), Var(v(5))));
 // }
 
 // Church booleans {
-#define T Lam(v(a), Lam(v(b), Var(v(a))))
-#define F Lam(v(c), Lam(v(d), Var(v(d))))
+#define T Lam(Lam(Var(v(2))))
+#define F Lam(Lam(Var(v(1))))
 
-#define NOT Lam(v(e), Appl(Appl(Var(v(e)), F), T))
-#define AND Lam(v(f), Lam(v(g), Appl(Appl(Var(v(f)), Var(v(g))), Var(v(f)))))
-#define OR  Lam(v(h), Lam(v(i), Appl(Appl(Var(v(h)), Var(v(h))), Var(v(i)))))
-#define XOR Lam(v(j), Lam(v(k), Appl(Appl(Var(v(j)), Appl(NOT, Var(v(k)))), Var(v(k)))))
+#define NOT Lam(Appl(Appl(Var(v(1)), F), T))
+#define AND Lam(Lam(Appl(Appl(Var(v(2)), Var(v(1))), Var(v(2)))))
+#define OR  Lam(Lam(Appl(Appl(Var(v(2)), Var(v(2))), Var(v(1)))))
+#define XOR Lam(Lam(Appl(Appl(Var(v(2)), Appl(NOT, Var(v(1)))), Var(v(1)))))
 
-#define IF Lam(v(l), Lam(v(m), Lam(v(n), Appl(Appl(Var(v(l)), Var(v(m))), Var(v(n))))))
+#define IF Lam(Lam(Lam(Appl(Appl(Var(v(3)), Var(v(2))), Var(v(1))))))
 
 // NOT {
 ASSERT_REDUCES_TO(Appl(NOT, T), F);
@@ -108,8 +116,8 @@ ASSERT_REDUCES_TO(Appl(Appl(XOR, F), F), F);
 // }
 
 // IF {
-ASSERT_REDUCES_TO(Appl(Appl(Appl(IF, T), Var(v(q))), Var(v(z))), Var(v(q)));
-ASSERT_REDUCES_TO(Appl(Appl(Appl(IF, F), Var(v(q))), Var(v(z))), Var(v(z)));
+ASSERT_REDUCES_TO(Appl(Appl(Appl(IF, T), Var(v(5))), Var(v(6))), Var(v(5)));
+ASSERT_REDUCES_TO(Appl(Appl(Appl(IF, F), Var(v(5))), Var(v(6))), Var(v(6)));
 // }
 
 // } (Church booleans)
