80526edd007c98d54789cd2f85ff5e1fa6a5c526
hirrolot <hirrolot@gmail.com>
2021-11-25 12:22:43 +0600

    Add `bench/filter_map.c`

--- scripts/bench.sh
+++ scripts/bench.sh
@@ -2,12 +2,13 @@
 
 bench() {
     echo $1
-    time gcc bench/$1 -ftrack-macro-expansion=0 -Iinclude
+    time gcc bench/$1 -ftrack-macro-expansion=0 -Iinclude -E -P >/dev/null
     echo ""
 }
 
-bench "compare_25_items.h"
-bench "list_of_63_items.h"
-bench "100_v.h"
-bench "100_call.h"
-bench "many_call_in_arg_pos.h"
+bench "compare_25_items.c"
+bench "list_of_63_items.c"
+bench "100_v.c"
+bench "100_call.c"
+bench "many_call_in_arg_pos.c"
+bench "filter_map.c"
